2ec99a35c72feb18ff9403788de2efe2c667d836
codebyaddro <codebyaddro@gmail.com>
2025-12-29 23:06:26 +0600

    build folder remaned

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/authStore";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://event-listing-system-backend-cba.vercel.app/api",
 });
 
 api.interceptors.request.use(
--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,5 @@ export default defineConfig({
   ],
   server: {
     port: 3000
-  },
-  build: {
-    outDir: 'frontend-code'
   }
 })
